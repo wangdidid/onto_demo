aed702f1aa103917bedb8e00fd77c528aed1b0d4
wangdidid <36759769+wangdidid@users.noreply.github.com>
2022-03-28 23:17:17 +0800

    Update web_demo.py

--- web_demo.py
+++ web_demo.py
@@ -6,8 +6,8 @@ st.title('农业食品技术评估  DEMO')
 
 sidebar_ = st.sidebar.radio(
     "Part",
-    ['The Second Domestication of Plants and Animals', 'Disruption and Adoption', 'Impacts and Implications', ],
-    index=0
+    ['The Second Domestication of Plants and Animals', 'Disruption and Adoption', 'Impacts and Implications', 'The New Language of Food'],
+    index=3
 )
 
 if sidebar_ == 'The Second Domestication of Plants and Animals':
@@ -44,6 +44,56 @@ if sidebar_ == 'Disruption and Adoption':
         }
     ''')
 
+if sidebar_ == 'Impacts and Implications': 
+    col1, col2, col3 = st.columns(3)
+    col1.metric("FOOD SECURITY", "70 %", "1.2 %")
+    col2.metric("ENVIRONMENTAL SUSTAINABILITY", "9 %", "-8 %")
+    col3.metric("ECONOMIC EQUITY", "86 %", "4 %")
+    
+#     st.selectbox('', options)
+                 
+if sidebar_ == 'The New Language of Food':
+    option = st.selectbox(
+     'The New Language of Food',
+     ('Cell-based Meat', 
+      'Chemical Synthesis', 
+      'Computational Biology',
+      'Enzyme',
+      'Fermentation Tank',
+      'Food-as-Software',
+      'Form factor',
+      'Fortification',
+      'Genetic Engineering',
+      'High-throughput Screening',
+      'Industrial Agriculture',
+      'Macro-organism',
+      'Metabolic Engineering',
+      'Micro-organism (microbe)',
+      'Modern Food',
+      'Mycoprotein',
+      'Plant-based Meat',
+      'Precision Agriculture',
+      'Precision Biology',
+      'Precision Fermentation',
+      'Precision-fermentation Enabled',
+      'Precision-fermentation Enhanced',
+      'Synthetic Biology',
+      'Systems Biology',
+     )
+    )
+
+    if option == 'Cell-based Meat':
+        st.write('Meat that is comprised of animal cells grown outside an animal in a bioreactor. These products are genetically identical to conventional animal products. Cell-based meat is also referred to by others as clean meat, lab-grown meat, cultured meat, or in-vitro meat.')
+    elif option == 'Chemical Synthesis': 
+        st.write('The construction of chemical compounds through a series of chemical reactions or physical manipulations to get from precursors (petrochemical or natural) to organic molecules. Synthesis is used to discover compounds with new physical or biological properties, to produce compounds that do not form naturally, or to make products in large quantities. Products created through chemical synthesis are typically referred to as synthetic or man-made and are alternatives to natural products.
+    else:
+        st.write('xxx')
+    color = st.select_slider(
+     '请选择该领域中项目与之相关的等级：',
+     options=['毫无关系', '部分相关', '涉及', '高度应用', '主题完全相符'])
+    st.write('该领域中，你认为项目与之相关的等级为：', color)
+             
+    
 
 df = pd.DataFrame(
      np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
